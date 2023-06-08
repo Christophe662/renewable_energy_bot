b251ec52d2dd8b344d8a98247cf4a66ff379b455
Christophe662 <132502536+Christophe662@users.noreply.github.com>
2023-06-08 16:48:43 +0200

    Update train.py

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from torch.utils.data import Dataset, DataLoader
 from chatbot2 import bag_of_words, tokenize, stem
 from model import NeuralNet
 
-with open('/home/ubuntu/renewable_energy_bot/intents.json', 'r') as f:
+with open('intents.Json', 'r') as f:
     intents = json.load(f)
 
 all_words = []
